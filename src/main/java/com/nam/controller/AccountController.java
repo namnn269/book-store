0df2d7d44a0a579406db932e6c9932669d8acde4
namnn269 <111518421+namnn269@users.noreply.github.com>
2022-12-12 11:24:06 +0700

    88

--- src/main/java/com/nam/controller/AccountController.java
+++ src/main/java/com/nam/controller/AccountController.java
@@ -22,7 +22,7 @@ import com.nam.service.IProfileService;
 @Controller
 @RequestMapping(value = "/account")
 public class AccountController {
-	// test git 77
+	// test git 88
 	@Autowired
 	IProfileService profileService;
 	@Autowired
